de3449ae562b5a3f60190ec63842b0d2fc1dd890
Lynne <thecoolone3@gmail.com>
2019-01-11 22:55:31 +1000

    added extract_toot function to functions.py

--- functions.py
+++ functions.py
@@ -60,3 +60,26 @@ def make_toot_markov(query = None):
 			"toot": toot,
 			"media": None
 		}
+
+def extract_toot(toot):
+	soup = BeautifulSoup(toot, "html.parser")
+	for lb in soup.select("br"): #replace <br> with linebreak
+		lb.insert_after("\n")
+		lb.decompose()
+
+	for p in soup.select("p"): #ditto for <p>
+		p.insert_after("\n")
+		p.unwrap()
+
+	for ht in soup.select("a.hashtag"): #make hashtags no longer links, just text
+		ht.unwrap()
+
+	for link in soup.select("a"): #ocnvert <a href='https://example.com>example.com</a> to just https://example.com
+		link.insert_after(link["href"])
+		link.decompose()
+
+	toot = soup.get_text()
+	text = re.sub("https://([^/]+)/(@[^ ]+)", r"\2@\1", text) #put mastodon-style mentions back in
+	text = re.sub("https://([^/]+)/users/([^ ]+)", r"@\2@\1", text) #put pleroma-style mentions back in
+	text = text.rstrip("\n") #remove trailing newline
+	return text
