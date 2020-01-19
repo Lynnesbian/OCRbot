bea09261d2baff221c206ccc4f7a0099e0f8c52b
Lynne <thecoolone3@gmail.com>
2020-01-19 21:49:47 +1000

    updated copyright info because why not

--- login.py
+++ login.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
-# toot downloader version two!!
-# Copyright (C) 2019 Lynne (@lynnesbian@fedi.lynnesbian.space)
+# OCRbot login code
+# Copyright (C) 2020 Lynne (@lynnesbian@fedi.lynnesbian.space)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
--- service.py
+++ service.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# Copyright (C) 2019 Lynne (@lynnesbian@fedi.lynnesbian.space)
+# Copyright (C) 2020 Lynne (@lynnesbian@fedi.lynnesbian.space)
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
